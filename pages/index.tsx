2981495948613852bf0fec0c4374f9c6d09bc198
tpessoa <tiagopessoa123@hotmail.com>
2023-02-15 22:26:11 +0000

    improve timer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import type { NextPage } from "next";
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import { Tab } from "@headlessui/react";
 import { formatClockTimer } from "../utils/utils";
+import { RiSettings4Fill } from "react-icons/ri";
 
 const buttons = ["pomodoro", "short break", "long break"];
 
 const Home: NextPage = () => {
   const [progress, setProgress] = useState<number>(100);
   const [currentTimer, setCurrentTimer] = useState<string>("00:00");
+  const [counterPaused, setCounterPaused] = useState<boolean>(true);
+  const [elapsedTime, setElapsedTime] = useState<number>(0);
 
   const strokeWidth = 4;
   const size = 100;
@@ -15,28 +18,61 @@ const Home: NextPage = () => {
   const arcLength = 2 * Math.PI * radius;
   const arcOffset = arcLength * ((100 - progress) / 100);
 
-  const currentDate = new Date();
-  const countDownDate = new Date().setMinutes(currentDate.getMinutes() + 1);
-  const startTimeLeft = countDownDate - new Date().getTime();
+  const counterTimeInput = 20;
+  const timeMetric = 1000; // 1 second
+  const timer = useRef(null);
 
-  useEffect(() => {
-    const timeCounter = setInterval(() => {
-      const timeleft = countDownDate - new Date().getTime();
+  const resetCountDownTimer = () => {
+    setCurrentTimer("00:00");
+    setProgress(100);
+    setElapsedTime(0);
+    setCounterPaused(true);
+  };
 
-      const minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-      // setMinutes(minutes);
-      // setSeconds(seconds);
-      console.log(minutes, seconds);
+  const refreshCountDownTimer = (date: number, initalTimeLeft: number) => {
+    const timeleft =
+      Math.round((date - new Date().getTime()) / timeMetric) * timeMetric;
 
-      setCurrentTimer(formatClockTimer(minutes, seconds));
-      setProgress((timeleft * 100) / startTimeLeft);
+    setCurrentTimer(formatClockTimer(timeleft));
+    setElapsedTime((previousValue) => previousValue + 1);
 
-      if (minutes === 0 && seconds === 0) {
-        clearInterval(timeCounter);
-      }
-    }, 1000);
-  }, []);
+    if (timeleft <= 0) {
+      clearInterval(timer.current);
+      resetCountDownTimer();
+    } else {
+      setProgress((timeleft * 100) / initalTimeLeft);
+    }
+  };
+
+  const startTimer = () => {
+    // console.log("Timer starting...");
+    setCounterPaused(false);
+
+    if (elapsedTime >= counterTimeInput) {
+      resetCountDownTimer();
+      return;
+    }
+
+    const countDownDate = new Date().setSeconds(
+      new Date().getSeconds() + counterTimeInput
+    );
+    const countDownDateWithElapsedTime = new Date().setSeconds(
+      new Date().getSeconds() + counterTimeInput - elapsedTime
+    );
+
+    const initalTimeLeft = countDownDate - new Date().getTime();
+    timer.current = setInterval(
+      () => refreshCountDownTimer(countDownDateWithElapsedTime, initalTimeLeft),
+      timeMetric
+    );
+  };
+
+  const pauseTimer = () => {
+    // console.log("Timer pausing...");
+    // console.log("Elapsed time: ", elapsedTime);
+    clearInterval(timer.current);
+    setCounterPaused(true);
+  };
 
   return (
     <div className="bg-dark-purple h-screen font-sans">
@@ -82,7 +118,7 @@ const Home: NextPage = () => {
           </Tab.List>
         </Tab.Group>
       </div>
-      <div className="relative w-full flex justify-center my-20 -rotate-90">
+      <div className="relative w-full flex justify-center my-24 ">
         <svg className="w-80 h-80 z-50" viewBox={`0 0 ${size} ${size}`}>
           <circle
             className="text-rose-400/90"
@@ -97,23 +133,34 @@ const Home: NextPage = () => {
             fill="transparent"
           />
         </svg>
-
-        <div className="absolute text-6xl font-bold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-gray-200 z-50 rotate-90">
+        <div className="absolute text-7xl font-semibold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-gray-200 z-50">
           {currentTimer}
+        </div>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 h-80 z-50 ">
+          <button
+            onClick={() => (counterPaused ? startTimer() : pauseTimer())}
+            className="flex items-end justify-center w-full"
+            style={{ height: "80%" }}
+          >
+            <span
+              className="text-gray-200 text-ll uppercase text-center"
+              style={{
+                letterSpacing: "1rem",
+                marginRight: "-1rem",
+              }}
+            >
+              {counterPaused ? "Start" : "Pause"}
+            </span>
+          </button>
         </div>
         <div className="absolute text-6xl font-bold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-900 border-gray-900 w-80 h-80 border-8 rounded-full z-10"></div>
         <div className="absolute font-bold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gradient-to-r from-gray-800/40 via-gray-900/40 to-gray-900 w-96 h-96 rounded-full z-0"></div>
       </div>
-      {/* <div
-          className="relative h-96 w-96 bg-gray-800 rounded-full border-gray-800/90 z-10 w-10"
-          style={{ borderWidth: "20px" }}
-        >
-          <div
-            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-80 w-80 rounded-full z-50 border-rose-400/90"
-            style={{ borderWidth: "10px" }}
-          ></div>
-        </div>
-      </div> */}
+      <div className="w-full flex justify-center">
+        <button className="w-12 h-12">
+          <RiSettings4Fill className="w-full h-full text-gray-500" />
+        </button>
+      </div>
     </div>
   );
 };
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,4 +1,7 @@
-export const formatClockTimer = (minutes: number, seconds: number) => {
+export const formatClockTimer = (timeleft: number) => {
+  const minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
   const minutesStr = minutes < 10 ? `0${minutes}` : `${minutes}`;
   const secondsStr = seconds < 10 ? `0${seconds}` : `${seconds}`;
 
